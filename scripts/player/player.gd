79d0555c1194920c2f7e065b43d7b0922239e416
Archo <busterrecornx@gmail.com>
2026-03-11 20:07:23 +0800

    started workin

---
+++ scripts/player/player.gd
@@ -0,0 +1,20 @@
+class_name Player extends CharacterBody2D
+
+var move_speed : float  = 100.0
+
+func _ready() -> void:
+	print("ready!")
+	pass
+
+func _process(_delta):
+	var direction : Vector2 = Vector2.ZERO
+	direction.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	direction.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+	
+	velocity = direction * move_speed
+	
+	pass
+	
+func _physics_process(_delta):
+	move_and_slide()
+	pass
